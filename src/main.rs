8b7269f90e5a38479a190ae5bc0480ae1a98328b
HankBh <TwHankBh@gmail.com>
2025-12-10 20:38:10 +0800

    ## 0.5.1
    ### 更改
    
    - 整理程式碼

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use log::{debug, error, info, trace, warn};
-use ptrs::ptrs;
+use positive_tool_rs::pt;
 use std;
 
 const PROJECT_NAME: &str = env!("CARGO_PKG_NAME");
 
 fn main() {
-    let project_path: std::path::PathBuf = ptrs::find_project_root_path(env!("CARGO_PKG_NAME"))
+    let project_path: std::path::PathBuf = pt::find_project_root_path(env!("CARGO_PKG_NAME"))
         .ok()
         .unwrap();
     let test_tmp_file_path: std::path::PathBuf =
         project_path.clone().join("tmp_test_build_logger.log");
-    ptrs::build_logger(test_tmp_file_path.clone(), None)
+    pt::build_logger(test_tmp_file_path.clone(), None)
         .ok()
         .unwrap();
     trace!("{}的測試日志<追蹤>", PROJECT_NAME);
